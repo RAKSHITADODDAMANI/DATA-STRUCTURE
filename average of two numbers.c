a1dbb5ace762dc0e5a9dafba771eec957a377236
RAKSHITADODDAMANI <130588095+RAKSHITADODDAMANI@users.noreply.github.com>
2024-03-14 13:24:18 +0530

    Add files via upload

---
+++ average of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    float x;
+    float y;
+    float avg;
+    printf("Enter the values of x,y\n");
+    scanf("%f%f",&x,&y);
+    avg=(x+y)/2;
+    printf("average=%f",avg);
+}
